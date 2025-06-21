324aa5e3a054fc6f8072b43214cd8eab70c0ae67
ajj-k <bouando4ha@docomo.ne.jp>
2025-06-22 00:16:30 +0900

    add: StopWatch

---
+++ StopWatch/StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by Kiyoshi Ohashi on 2025/06/21.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
