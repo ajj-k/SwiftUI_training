6ac743d8c24ab38a1f08d2809be083e0928caf15
ajj-k <bouando4ha@docomo.ne.jp>
2025-06-28 23:53:53 +0900

    add: Gacha

---
+++ Gacha/Gacha/GachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachaApp.swift
+//  Gacha
+//
+//  Created by Kiyoshi Ohashi on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct GachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
