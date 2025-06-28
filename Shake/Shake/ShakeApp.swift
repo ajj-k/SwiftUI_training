ce0d724e1273bb186c36e07f98f91ac2aa7d7bcf
ajj-k <bouando4ha@docomo.ne.jp>
2025-06-29 02:34:00 +0900

    add: Shake

---
+++ Shake/Shake/ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by Kiyoshi Ohashi on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
